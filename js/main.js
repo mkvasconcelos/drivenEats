7b5277086a8ceece1671246a407fe07642ee0e6e
mkvasconcelos <mateuskvasconcelos@gmail.com>
2022-10-14 09:11:00 -0300

    Ajuste da função finish()

--- js/main.js
+++ js/main.js
@@ -36,36 +36,20 @@ function finish() {
     let body = document.body;
     let footer = body.children[2];
     let button_footer = footer.children[0].children[0].children[0];
-    button_footer.style.background = '#32B72F';
-    button_footer.style.border = "none";
+    button_footer.classList.add("button_footer");
     button_footer.innerHTML = "Fechar pedido";
-    button_footer.style.fontWeight = "700";
     button_footer.disabled = false;
 }
 
-// function send() {
-//     let name = prompt("Qual seu nome?");
-//     let address = prompt("Qual seu endereço?");
-//     let total = (Number(receipt[0]['value']) + Number(receipt[1]['value']) + Number(receipt[2]['value'])).toFixed(2);
-//     total = `${total.split(".")[0]},${total.split(".")[1]}`;
-//     let text = `Olá, gostaria de fazer o pedido: \n - Prato: ${receipt[0]['order']}\n - Bebida: ${receipt[1]['order']} \n - Sobremesa: ${receipt[2]['order']} \n Total: R$ ${total} \n\n Nome: ${name}\n Endereço: ${address}`;
+// function finish() {
 //     let body = document.body;
-//     let header = body.children[0];
-//     let main = body.children[1];
 //     let footer = body.children[2];
-//     header.style.opacity = "0.1";
-//     main.style.opacity = "0.1";
-//     footer.style.opacity = "0.1";
-//     let nav = body.children[3];
-//     nav.style.display = 'flex';
-//     nav.children[1].children[0].innerText = receipt[0]['order'];
-//     nav.children[1].children[1].innerText = `${receipt[0]['value'].split(".")[0]},${receipt[0]['value'].split(".")[1]}`;
-//     nav.children[2].children[0].innerText = receipt[1]['order'];
-//     nav.children[2].children[1].innerText = `${receipt[1]['value'].split(".")[0]},${receipt[1]['value'].split(".")[1]}`;
-//     nav.children[3].children[0].innerText = receipt[2]['order'];
-//     nav.children[3].children[1].innerText = `${receipt[2]['value'].split(".")[0]},${receipt[2]['value'].split(".")[1]}`;
-//     nav.children[4].children[1].innerText = `R$ ${total}`;
-//     finalText = text;
+//     let button_footer = footer.children[0].children[0].children[0];
+//     button_footer.style.background = '#32B72F';
+//     button_footer.style.border = "none";
+//     button_footer.innerHTML = "Fechar pedido";
+//     button_footer.style.fontWeight = "700";
+//     button_footer.disabled = false;
 // }
 
 function send() {
@@ -100,18 +84,6 @@ function send_bonus() {
     let nav = body.children[3];
     nav.children[5].href = wpp;
 }
-
-// function cancel() {
-//     let body = document.body;
-//     let header = body.children[0];
-//     let main = body.children[1];
-//     let footer = body.children[2];
-//     header.style.opacity = "1.0";
-//     main.style.opacity = "1.0";
-//     footer.style.opacity = "1.0";
-//     let nav = body.children[3];
-//     nav.style.display = 'none';
-// }
 
 function cancel() {
     let body = document.body;
